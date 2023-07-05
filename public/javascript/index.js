ce7d4418ee92693c5626c43cdc104c025fb11922
technologyaava@gmail.com <technologyaava@gmail.com>
2023-07-05 14:38:44 +0545

    file change

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -18,3 +18,22 @@ document.getElementById('ytvdoshow').addEventListener('click', function(){
 document.querySelector(".closebtn").addEventListener('click', function(){
     document.querySelector('.popup').style.display = 'none';
 })
+
+
+
+//  log in or what ever pop up box for 3 sec 
+
+const pop_Up_Container = document.getElementById('pop_Up_Container');
+const close = document.getElementById('close')
+
+
+window.addEventListener('load',()=>{
+   setTimeout(() => {
+    pop_Up_Container.style.display= "flex";
+    
+   }, 2000);
+})
+
+close.addEventListener('click',()=>{
+    pop_Up_Container.style.display = "none";
+})
