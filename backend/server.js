d4ce77757b60d70f9ba25b1aa049179b501c4bb2
sunil <logins697@gmail.com>
2024-04-08 00:39:42 +0545

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -147,6 +147,12 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port no : ${PORT}`);
+// });
+
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port no : ${PORT}`);
 });
+
